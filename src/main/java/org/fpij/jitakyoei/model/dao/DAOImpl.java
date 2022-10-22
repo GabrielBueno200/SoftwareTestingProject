abda23a6b1a44d1a5ea6c73fcd1e8d799f3f822b
GabrielBueno200 <gabrielbueno200@gmail.com>
2022-10-22 20:04:20 -0300

    downgrade db4o

--- src/main/java/org/fpij/jitakyoei/model/dao/DAOImpl.java
+++ src/main/java/org/fpij/jitakyoei/model/dao/DAOImpl.java
@@ -8,7 +8,6 @@ import org.fpij.jitakyoei.util.DatabaseManager;
 
 import com.db4o.ObjectSet;
 import com.db4o.ext.ExtObjectContainer;
-import com.db4o.query.Query;
 
 public class DAOImpl<E> implements DAO<E> {
 
@@ -16,65 +15,67 @@ public class DAOImpl<E> implements DAO<E> {
 	private Class<E> clazz;
 	private Validator<E> validator;
 	private boolean useEquals;
-
-	public DAOImpl(Class<E> clazz, Validator<E> val, boolean comp) {
+	
+	public DAOImpl(Class<E> clazz, Validator<E> val, boolean comp){
 		this.validator = val;
 		this.useEquals = comp;
 		this.clazz = clazz;
 	}
-
-	public DAOImpl(Class<E> clazz, boolean useEquals) {
+	
+	public DAOImpl(Class<E> clazz, boolean useEquals){
 		this.validator = new DefaultValidator<E>();
 		this.useEquals = useEquals;
 		this.clazz = clazz;
 	}
-
-	public DAOImpl(Class<E> clazz) {
+	
+	public DAOImpl(Class<E> clazz){
 		this.validator = new DefaultValidator<E>();
 		this.clazz = clazz;
 	}
-
+	
 	@Override
 	public synchronized boolean save(E object) {
 		System.out.println("DAOImpl.save()");
 		System.out.println(object);
-		if (validator.validate(object)) {
-			db.set(object);
+		if(validator.validate(object)){
+			db.store(object);
 			db.commit();
 			return true;
-		} else {
+		}
+		else{
 			return false;
 		}
 	}
-
+	
 	@Override
 	public synchronized void delete(E object) {
 		db.delete(object);
 		db.commit();
 	}
-
+	
 	@Override
 	public List<E> list() {
 		List<E> objects = new ArrayList<E>();
-		ObjectSet result = db.query(clazz);
-		while (result.hasNext()) {
-			objects.add((E) result.next());
+		ObjectSet<E> result = db.queryByExample(clazz);
+		while(result.hasNext()){
+			objects.add((E)result.next());
 		}
 		return objects;
 	}
-
+	
 	@Override
-	public E get(E object) throws IllegalArgumentException {
-		List<E> objectList = db.query(clazz);
-		if (useEquals) {
-			for (E each : objectList) {
-				if (each.equals(object)) {
+	public E get(E object) throws IllegalArgumentException{
+		List<E> objectList = db.queryByExample(clazz);
+		if(useEquals){
+			for(E each: objectList){
+				if(each.equals(object)){
 					return each;
 				}
 			}
-		} else {
+		}
+		else{
 			int index = objectList.indexOf(object);
-			if (index >= 0) {
+			if(index >= 0){
 				return objectList.get(index);
 			}
 		}
@@ -84,20 +85,15 @@ public class DAOImpl<E> implements DAO<E> {
 	@Override
 	public List<E> search(E object) {
 		List<E> objects = new ArrayList<E>();
-		Query query = db.query();
-                query.constrain(object);
-                
-                ObjectSet result = query.execute();
-                
-		while (result.hasNext()) {
-			objects.add((E) result.next());
+		ObjectSet<E> result = db.queryByExample(object);
+		while(result.hasNext()){
+			objects.add((E)result.next());
 		}
-		return objects;
+		return objects;		
 	}
 
 	/**
-	 * Classe utilizada caso o nenhuma classe Validador seja fornecida na
-	 * instanciação.
+	 * Classe utilizada caso o nenhuma classe Validador seja fornecida na instanciação.
 	 */
 	public class DefaultValidator<T> implements Validator<T> {
 		@Override

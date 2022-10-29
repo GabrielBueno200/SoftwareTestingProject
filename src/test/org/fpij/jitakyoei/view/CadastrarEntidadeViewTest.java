633b5c602c824d5266fa1e4a07332089279a9746
GABRIEL BUENO V R DE OLIVEIRA <unifgaboliveira>
2022-10-28 22:12:42 -0300

    adicionado testes da view de cadastrar entidade

---
+++ src/test/org/fpij/jitakyoei/view/CadastrarEntidadeViewTest.java
@@ -0,0 +1,99 @@
+package org.fpij.jitakyoei.view;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+import java.awt.Component;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+import org.fpij.jitakyoei.builders.EntidadeMockBuilder;
+import org.fpij.jitakyoei.facade.AppFacade;
+import org.fpij.jitakyoei.model.beans.Entidade;
+import org.fpij.jitakyoei.view.forms.EntidadeForm;
+import org.fpij.jitakyoei.view.gui.EntidadeCadastrarPanel;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedConstruction;
+import org.mockito.MockedStatic;
+
+public class CadastrarEntidadeViewTest {
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"", " "})
+    public void Cadastrar_EntidadeComNomeVazioOuNulo_ExibirAlertaDeCampoObrigatorio(String nomeNullOrEmpty){
+        Entidade entidade = new EntidadeMockBuilder()
+                        .WithNome(nomeNullOrEmpty)
+                        .Build();
+        
+        TestarEntidadeComCampoVazioOuInvalido(entidade, "O campo 'nome' é obrigatório");
+    }
+    
+    private void TestarEntidadeComCampoVazioOuInvalido(Entidade entidade, String expectedErrorMessage) {
+        // Arrange
+        MainAppView mainAppViewMock = mock(MainAppView.class);
+
+        AppFacade facadeMock = mock(AppFacade.class);
+        doNothing().when(facadeMock).createEntidade(entidade);
+
+        try (MockedConstruction<EntidadeCadastrarPanel> panelMockScope = mockConstruction(
+            EntidadeCadastrarPanel.class, 
+            (entidadeCadastrarPanelMock, context) -> AddButtonsMock(entidadeCadastrarPanelMock))
+        ){
+            try (MockedConstruction<EntidadeForm> formMockScope = mockConstruction(
+                EntidadeForm.class, 
+                (entidadeFormMock, context) -> {
+                    when(entidadeFormMock.getEntidade()).thenReturn(entidade);
+                })
+            ){
+                EntidadeCadastrarView sut = new EntidadeCadastrarView(mainAppViewMock);
+                sut.registerFacade(facadeMock);
+
+                JButton cadastrarButton = ((EntidadeCadastrarPanel)sut.getGui()).getCadastrarEntidade();
+                
+                try (MockedStatic<JOptionPane> optionPaneMock = mockStatic(JOptionPane.class)) 
+                {
+                    // Act
+                    cadastrarButton.doClick();
+        
+                    // Assert
+                    optionPaneMock.verify(
+                            () -> JOptionPane.showMessageDialog(
+                                    any(Component.class),
+                                    eq(expectedErrorMessage),
+                                    anyString(),
+                                    eq(JOptionPane.ERROR_MESSAGE)),
+                            times(1));
+                }          
+            }
+        }
+    }
+
+    private void AddButtonsMock(EntidadeCadastrarPanel cadastrarEntidadePanelMock)
+    {
+        // Mock botão de cadastrar
+        JButton cadastrarButtonSpy = spy(new JButton());
+        doCallRealMethod().when(cadastrarButtonSpy).addActionListener(any(EntidadeCadastrarView.CadastrarActionHandler.class));
+        when(cadastrarEntidadePanelMock.getCadastrarEntidade()).thenReturn(cadastrarButtonSpy);
+        
+        // Mock botão de cancelar
+        JButton cancelarButtonSpy = spy(new JButton());
+        doNothing().when(cancelarButtonSpy).addActionListener(any(EntidadeCadastrarView.CancelarActionHandler.class));
+        when(cadastrarEntidadePanelMock.getCancelar()).thenReturn(cancelarButtonSpy);
+
+        // mock setVisible
+        doNothing().when(cadastrarEntidadePanelMock).setVisible(anyBoolean());
+    }
+}

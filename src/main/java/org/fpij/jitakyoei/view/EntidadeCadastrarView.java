6de3b280ae73a2a110dd7d4b8325943fcea1f810
henriquevital00 <henriquevital1000@hotmail.com>
2022-11-11 18:20:09 -0300

    Criado validacao de campos para adicionar entidade

--- src/main/java/org/fpij/jitakyoei/view/EntidadeCadastrarView.java
+++ src/main/java/org/fpij/jitakyoei/view/EntidadeCadastrarView.java
@@ -2,11 +2,14 @@ package org.fpij.jitakyoei.view;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import org.fpij.jitakyoei.facade.AppFacade;
+import org.fpij.jitakyoei.model.beans.Entidade;
 import org.fpij.jitakyoei.view.forms.EntidadeForm;
 import org.fpij.jitakyoei.view.gui.EntidadeCadastrarPanel;
 
@@ -45,9 +48,26 @@ public class EntidadeCadastrarView implements ViewComponent {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			try {
-				facade.createEntidade(entidadeForm.getEntidade());
-				JOptionPane.showMessageDialog(gui, "Entidade cadastrada com sucesso!");
-				parent.removeTabPanel(gui);
+                                Entidade entidade = entidadeForm.getEntidade();
+                                String regexCnpj = "^([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})$";
+                                String regexTelefone = "^\\([0-9]{2}\\)[0-9]{4,5}-[0-9]{4}$";
+                                String regexNome = "^[a-zA-Z\\s]+$";
+                                Matcher matchCnpj = Pattern.compile(regexCnpj).matcher(entidade.getCnpj());
+                                Matcher matchTelefone1 = Pattern.compile(regexTelefone).matcher(entidade.getTelefone1());
+                                Matcher matchNome = Pattern.compile(regexNome).matcher(entidade.getNome());
+                                if (entidade.getNome().isBlank() && entidade.getCnpj().isBlank() && entidade.getTelefone1().isBlank()){
+                                    if (matchCnpj.matches() && matchTelefone1.matches() && matchNome.matches()){
+                                        facade.createEntidade(entidadeForm.getEntidade());
+                                        JOptionPane.showMessageDialog(gui, "Entidade cadastrada com sucesso!");
+                                        parent.removeTabPanel(gui);
+                                    }
+                                    else{
+                                        JOptionPane.showMessageDialog(gui, "Campos NOME/CNPJ/TELEFONE possuem formato incorreto!");
+                                    }
+                                }
+                                else{
+                                    JOptionPane.showMessageDialog(gui, "Campos obrigatórios não preenchidos!");
+                                }
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
